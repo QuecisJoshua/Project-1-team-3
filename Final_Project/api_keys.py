7d3a2c8dfaadcc6fc0b4ebf4e238e7f8d950e5a9
Quecis <55006890+QuecisJoshua@users.noreply.github.com>
2020-04-10 20:57:58 -0700

    Final Project
    Final Project

---
+++ Final_Project/api_keys.py
@@ -0,0 +1,9 @@
+# OpenWeatherMap API Key
+weather_api_key = "c440edd33f100e0cde207440aa46bcca"
+
+# Google API Key
+g_key = "AIzaSyAfWeu4Z7EUlFYTcLduUsHgtL7OS3F7E5w"
+
+
+# Census API KEY
+census_key = "eb5784e70ad0bf65d27520419c7caafbb7366b48"
